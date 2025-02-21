2e8f0b65790a82fa432b11f335cc64e7ab9d7cdc
mameluka <robertogonzalez.potocoto@gmail.com>
2025-02-21 14:49:02 -0300

    lad

---
+++ Blog/Articulos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from Articulos.models import Entrada
+# Create your views here.
+
+
+def home(request):
+    articulos = Entrada.objects.all()
+    return render(request, "bienvenida.html", {"articulos": articulos})
